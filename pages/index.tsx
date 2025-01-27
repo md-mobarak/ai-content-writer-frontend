65ae01446b81261e3f03c6632689cfa88386860f
md-mobarak <mdmobarakhossen782@gmail.com>
2025-01-27 15:52:44 +0600

    updated code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+
+
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import TrustedBySection from "@/components/TrustedBySection";
@@ -13,15 +13,7 @@ import Footer from "@/components/Footer";
 // import { Navbar } from "@/components/Navbar";
 // import { Hero } from "@/components/Hero";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
